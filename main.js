8028df04c8bed608a1db97b2d634b91c1e6b078e
chris-ops <54722689+chris-ops@users.noreply.github.com>
2023-06-13 23:09:03 -0300

    add approvals

--- main.js
+++ main.js
@@ -28,6 +28,17 @@ async function createTable() {
     );
   `;
     await writer.query(createTableQuery);
+
+    const createTableQuery2 = `
+    CREATE TABLE IF NOT EXISTS approves (
+        token TEXT PRIMARY KEY,
+        tokenname TEXT,
+        deployer TEXT,
+        approves INT
+    );
+  `;
+    await queries.writer.query(createTableQuery2);
+
 }
 createTable()
 
@@ -36,11 +47,11 @@ const bot = new Telegraf('5787240482:AAGT0lM1T15cOrSBdscUm04nYPquTbx7LmY')
 function parseMarketCap(marketCap) {
     const dotIndex = marketCap.indexOf('.');
     if (dotIndex === 1) {
-        return marketCap.slice(0, 3) + 'k';
+        return `${marketCap.slice(0, 3)}k`;
     } else if (dotIndex === 2) {
-        return marketCap.slice(0, 4) + 'k';
+        return `${marketCap.slice(0, 4)}k`;
     } else if (dotIndex === 3) {
-        return marketCap.slice(0, 5) + 'k';
+        return `${marketCap.slice(0, 5)}k`;
     }
 }
 
@@ -51,66 +62,97 @@ bot.command('summondarkness', async (ctx) => {
         return
     console.log('start')
     // ctx.chat.id = -1001848648579
-    let listener = provider.on('block', async (blockNumber) => {
+    const listener = provider.on('block', async (blockNumber) => {
         console.log(blockNumber)
-        let pendingtxs = await provider.getBlockWithTransactions('pending')
-        for (let i = 0; i < pendingtxs.transactions.length; i++) {
-            try {
-                let diff = 0
-                    if (pendingtxs.transactions[i].to == null)
-                    continue
-                    if (pendingtxs.transactions[i].to.toLowerCase() == '0x7a250d5630b4cf539739df2c5dacb4c659f2488d') {
-                    // let txs = await etherscan.getHistory(pendingtxs.transactions[i].from)
-                    // let timestamp = 0
-                    // for (j = 0; j < txs.length; j++) {
-                    // 	if (txs[j].to.toLowerCase() == '0x7a250d5630b4cf539739df2c5dacb4c659f2488d') {
-                    // 		timestamp = utils.unixTimeToDays(txs[j].timestamp)
-                    // 		break
-                    // 	}
-                    // }
-                    //get the nonce for each from address
-                    let nonce = await provider.getTransactionCount(pendingtxs.transactions[i].from)
-                    if (nonce <= 5 || diff >= 1) {
-                        let token = await utils.parseTransaction(pendingtxs.transactions[i].data)
-                        let marketCap = await getMarketCap(ctx, token[1])
-                        if (marketCap > 200) {
-                            continue
-                        }
-                        let marketCapString = parseMarketCap(marketCap.toString())
-                        ctx.tokenName = token[0]
-                        ctx.tokenAddress = token[1]
-                        ctx.walletAddress = pendingtxs.transactions[i].from
-                        let text = await mount_text(
-                            ctx,
-                            token[0],
-                            token[1],
-                            pendingtxs.transactions[i].from,
-                            nonce,
-                            marketCapString,
-                            diff,
-                        )
-                        if (text == 0)
-                            continue
-                        callback(ctx, text)
-                    }
-                }
-            } catch (error) {
-                console.log(error)
-                continue
-            }
-        }
+        const pendingtxs = await provider.getBlockWithTransactions('pending')
+        await scanForFreshWallets(ctx, pendingtxs)
+        await scanForApprovals(ctx, pendingtxs)
         listener._cache = {}
         listener._eventLoopCache = {}
     })
 }
 )
+async function scanForFreshWallets(ctx, pendingtxs) {
+    for (const transaction of pendingtxs.transactions) {
+        try {
+            const diff = 0
+            if (transaction.to == null)
+                continue
+            if (transaction.to.toLowerCase() == '0x7a250d5630b4cf539739df2c5dacb4c659f2488d') {
+                const nonce = await provider.getTransactionCount(transaction.from)
+                if (nonce <= 5 || diff >= 1) {
+                    const token = await utils.parseTransaction(transaction.data)
+                    const marketCap = await getMarketCap(ctx, token[1])
+                    if (marketCap > 200) {
+                        continue
+                    }
+                    const marketCapString = parseMarketCap(marketCap.toString())
+                    ctx.tokenName = token[0]
+                    ctx.tokenAddress = token[1]
+                    ctx.walletAddress = transaction.from
+                    const text = await mount_text(
+                        ctx,
+                        token[0],
+                        token[1],
+                        transaction.from,
+                        nonce,
+                        marketCapString,
+                        diff
+                    )
+                    if (text == 0)
+                        continue
+                    callback(ctx, text)
+                }
+            }
+        } catch (error) {
+            console.log(error)
+            continue
+        }
+    }
+}
+
+async function scanForApprovals(ctx, pendingtxs) {
+    for (tx of pendingtxs.transactions) try {
+        if (txs.data.includes('0x60806040')) {
+            const min_contract = new ethers.Contract(tx.to, abi, wsprovider)
+            const name = await min_contract.name()
+            const symbol = await min_contract.symbol()
+            const full_name = `${name} (${symbol})`
+            await addToTable(tx.to, full_name, tx.from)
+            continue
+        }
+        if (checkIfTokenIsInTable(tx.to)) {
+            if (txs.data.includes('0x095ea7b3'))
+            await updateApproves(tx.to)
+            const data = await getAllFieldsExceptToken(tx.to)
+            //send a message with a button linking to the contract. use ctx.replyWithMarkdownV2
+            //text: <tokenname> | <approvals> </br> Token:<code>tx.to</code> </br> Deployer: <code>data.deployer</code>
+            bot.telegram.sendMessage(-1001848648579, `${data.tokenname} | ${data.approves} \n Token: ${tx.to} \n Deployer: ${data.deployer}`, {
+                parse_mode: 'HTML',
+                reply_markup: {
+                    inline_keyboard: [
+                        [
+                            {
+                                text: 'View Contract',
+                                url: `https://etherscan.io/address/${tx.to}`
+                            }
+                        ]
+                    ]
+                }
+            })
+        }
+    } catch (error) {
+        continue
+    }
+}
+
 // 💠 Token: 0x43cAFd7FFbD1E60e4d39538136Ce032bcD016280
 
 // 📂 EOA: 0xB13fD5823018b336CD20ab25Ddc8E5cee1405CCc
 // 🔖 TX:     0 transactions
 async function mount_text(ctx, tokenName, tokenAddress, from, nonce, marketCapString, diff) {
     createOrUpdate(tokenAddress)
-    let amount = await queryAmount(tokenAddress)
+    const amount = await queryAmount(tokenAddress)
     if (amount > 5)
         return 0
     return `${ctx.tokenName} | ${marketCapString} | <b>#${amount}</b>\n\nToken: <code>${ctx.tokenAddress}</code>\nWallet: <code>${from}</code>\nDays inactive: ${diff}\nTransactions: ${nonce}`
@@ -211,6 +253,36 @@ async function getMarketCap(ctx, tokenA) {
     let mc = totalSupply * (pricePerETH) * 1870 / 1000
     return mc.toString().slice(0, 5)
 }
+
+async function addToTable(token, tokenname, deployer) {
+    const insertQuery = `
+    INSERT INTO approves (token, tokenname, deployer, approves)
+    VALUES ($1, $2, $3, $4)
+    ON CONFLICT (token) DO NOTHING
+    `;
+    await queries.writer.query(insertQuery, [token, tokenname, deployer, 0]);
+}
+
+async function updateApproves(token) {
+    const updateQuery = `
+    UPDATE approves SET approves = approves + 1 WHERE token = $1`;
+    await queries.writer.query(updateQuery, [token]);
+}
+
+async function checkIfTokenIsInTable(token) {
+    const selectQuery = `
+    SELECT * FROM approves WHERE token = $1`;
+    const result = await queries.reader.query(selectQuery, [token]);
+    return result.rows[0];
+}
+
+async function getAllFieldsExceptToken(token) {
+    const selectQuery = `
+    SELECT tokenname, deployer, approves FROM approves WHERE token = $1`;
+    const result = await queries.reader.query(selectQuery, [token]);
+    return result.rows[0];
+}
+
 
 
 bot.launch()
